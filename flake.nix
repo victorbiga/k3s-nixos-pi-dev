a0277b682343cec40f90158a426f5b9a19443fec
victorbiga <victor.biga@gmail.com>
2025-08-28 11:46:32 +0100

    updates for dev

--- flake.nix
+++ flake.nix
@@ -2,41 +2,47 @@
   description = "NixOS Raspberry Pi configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     raspberry-pi-nix.url = "github:nix-community/raspberry-pi-nix";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, raspberry-pi-nix }: {
-    # Helper function to generate NixOS configurations for nodes
-    mkNode4 = nodeConfig: nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";  # Architecture defined once
-      modules = [
-        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-        nixos-hardware.nixosModules.raspberry-pi-4
-        "${nixpkgs}/nixos/modules/profiles/minimal.nix"
-        ./shared/config.nix  # Common configurations for all nodes
-        ./shared/rpi4.nix  # Common configurations for all nodes
-        nodeConfig  # Specific node configuration
-      ];
-    };
-    mkNode5 = nodeConfig: nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";  # Architecture defined once
-      modules = [
-        raspberry-pi-nix.nixosModules.raspberry-pi
-        raspberry-pi-nix.nixosModules.sd-image
-        "${nixpkgs}/nixos/modules/profiles/minimal.nix"
-        ./shared/config.nix  # Common configurations for all nodes
-        ./shared/rpi5.nix
-        nodeConfig  # Specific node configuration
-      ];
-    };
+  outputs = { self, nixpkgs, nixos-hardware, raspberry-pi-nix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [];
+        };
+        # Helper function to generate NixOS configurations for nodes
+        mkNode = { nodeConfig, rpiVersion ? "rpi5" }:
+          nixpkgs.lib.nixosSystem {
+            system = "aarch64-linux"; # Architecture defined once
+            modules = [
+              raspberry-pi-nix.nixosModules.raspberry-pi
+              raspberry-pi-nix.nixosModules.sd-image
+              "${nixpkgs}/nixos/modules/profiles/minimal.nix"
+              ./shared/config.nix
+            ] ++ (if rpiVersion == "rpi4" then [
+              nixos-hardware.nixosModules.raspberry-pi-4
+              ./shared/rpi.nix
+            ] else [
+              ./shared/rpi.nix
+            ]) ++ [ nodeConfig ];
+          };
+      in {
+        # Formatter
+        formatter = pkgs.nixpkgs-fmt;
 
-    nixosConfigurations = {
-      # Node configurations using the mkNode helper function
-      dev = self.mkNode5 ./nodes/dev.nix;
-      dev-worker-1 = self.mkNode5 ./nodes/dev-worker-1.nix;
-      dev-worker-2 = self.mkNode4 ./nodes/dev-worker-2.nix;
+        nixosConfigurations = {
+          # Node configurations using the mkNode helper function
+          dev = mkNode { nodeConfig = ./nodes/dev.nix; };
+          dev-worker-1 = mkNode { nodeConfig = ./nodes/dev-worker-1.nix; };
+          dev-worker-2 = mkNode {
+            nodeConfig = ./nodes/dev-worker-2.nix;
+            rpiVersion = "rpi4";
+          };
     };
   };
 }
