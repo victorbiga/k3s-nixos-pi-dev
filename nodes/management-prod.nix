ddc91bc02d7a27d2f6e2e1f729ae73da0dbccb93
victorbiga <victor.biga@gmail.com>
2025-10-11 13:39:06 +0100

    feat: add prods

---
+++ nodes/management-prod.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, ... }:
+
+let
+  k3sConfig = import ../shared/k3s-config.nix {
+    inherit pkgs;
+    role = "server";
+  };
+in
+{
+  imports = [ k3sConfig ];
+
+  config = {
+
+    networking = {
+      hostName = "management-prod";
+      interfaces.end0.ipv4.addresses = [{
+        address = "10.0.0.31";
+        prefixLength = 24;
+      }];
+    };
+  };
+}
