f7cfa1d57fcda96a23d36ea5c9b29f5076d0ddcc
Victor Biga <victor.biga@gmail.com>
2025-10-13 12:23:34 +0100

    Update config.nix

--- shared/config.nix
+++ shared/config.nix
@@ -93,7 +93,7 @@
       syntaxHighlighting.enable = true;
       promptInit = ''
         eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init zsh --config ${../cluster.omp.json})"
-        eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init zsh > "$HOME/.zshrc
+        eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh completion zsh > "$HOME/.zshrc
       '';
     };
     neovim = {
