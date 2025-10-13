4191d8a592dfc793cb63fc81d388883ddcb8704b
Victor Biga <victor.biga@gmail.com>
2025-10-13 15:37:58 +0100

    Update k3s-config.nix

--- shared/k3s-config.nix
+++ shared/k3s-config.nix
@@ -7,6 +7,9 @@
     # Hardcoded server address for agents
     serverAddr = if role == "agent" then "https://10.0.0.32:6443" else "";
 
+    # Only add token for agents, let master node bootstrap itself
+    tokenFile = if role == "agent" then "/etc/k3s/token" else null;
+
     # Include additional flags conditionally based on the role
     extraFlags = if role == "server" then [ "--disable=traefik" ] else [];
 
